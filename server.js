290702a660998438108bcad8c1938fb612b3d7e3
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-07-28 13:30:36 +0200

    Splited HTML user interface on independent module

--- server.js
+++ server.js
@@ -1,40 +1,8 @@
 // Requirements
-var express = require('express')
-  , jqtpl   = require("jqtpl");
+var express = require('express');
 
 app = express.createServer()
+app.listen(8000);
 
 require("./connect_websockets.js");
-
-
-// App Stuff
-app.use('/public', express.static(__dirname + '/public'));
-app.listen(8000);
-app.set("view engine", "html");
-app.set("view options", {layout: false});
-app.register(".html", jqtpl.express);
-
-app.get('/', function(req, res)
-{
-  res.redirect('/' + randomString());
-});
-
-app.get('/:hash', function(req, res)
-{
-  res.render(__dirname + '/public/index', {domain: 'localhost:8888'});
-});
-
-// Utilities
-function randomString()
-{
-	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghijklmnopqrstuvwxyz";
-	var string_length = 8;
-	var randomstring = '';
-	for(var i=0; i<string_length; i++)
-	{
-		var rnum = Math.floor(Math.random() * chars.length);
-		randomstring += chars.substring(rnum,rnum+1);
-	}
-
-	return randomstring;
-}
+require("./ui_html.js");
---
+++ ui_html.js
@@ -0,0 +1,33 @@
+// App Stuff
+var express = require('express')
+  , jqtpl   = require("jqtpl");
+
+app.use('/public', express.static(__dirname + '/public'));
+app.set("view engine", "html");
+app.set("view options", {layout: false});
+app.register(".html", jqtpl.express);
+
+app.get('/', function(req, res)
+{
+  res.redirect('/' + randomString());
+});
+
+app.get('/:hash', function(req, res)
+{
+  res.render(__dirname + '/public/index', {domain: 'localhost:8888'});
+});
+
+// Utilities
+function randomString()
+{
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghijklmnopqrstuvwxyz";
+	var string_length = 8;
+	var randomstring = '';
+	for(var i=0; i<string_length; i++)
+	{
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+
+	return randomstring;
+}
