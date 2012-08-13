71d6cf9bbbd1ee9e7ec59746f01386a477fc5356
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-08-13 14:26:16 +0200

    Using url hash to define websocket room

--- ui_html.js
+++ ui_html.js
@@ -10,26 +10,5 @@ app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res)
 {
-  res.redirect('/' + randomString());
-});
-
-app.get('/:hash', function(req, res)
-{
   res.sendfile(__dirname + '/public/index.html');
-});
-
-// Utilities
-function randomString()
-{
-	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghijklmnopqrstuvwxyz";
-	var string_length = 8;
-
-	var randomstring = '';
-	for(var i=0; i<string_length; i++)
-	{
-		var rnum = Math.floor(Math.random() * chars.length);
-		randomstring += chars.substring(rnum,rnum+1);
-	}
-
-	return randomstring;
-}
+})
