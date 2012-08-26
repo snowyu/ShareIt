b887f91d079fc3c5170701efc419075480c78df1
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-08-27 01:40:43 +0200

    Resorted protocol functions

--- html_advanced/js/shareit/protocol.js
+++ html_advanced/js/shareit/protocol.js
@@ -6,36 +6,42 @@ function Conn_init(ws_url, host, onconnect, onsuccess, onerror)
 			if(onconnect)
 				onconnect(connection);
 
-			// Host
-			connection.on('transfer.query_chunk', host.transfer_query_chunk)
+			// Files list
 
-			connection.transfer_send_chunk = function(socketId, filename, chunk, data)
-			{
-				connection.emit('transfer.send_chunk', socketId, filename, chunk, data);
-			}
-
-			// Peer
-			connection.on('fileslist.update', function(socketId, data)
-			{
-				host.fileslist_update(socketId, JSON.parse(data))
-			})
-			connection.on('transfer.send_chunk', function(socketId, filename, chunk, data)
-			{
-				host.transfer_send_chunk(socketId, filename, parseInt(chunk), data)
-			})
-
-            connection.fileslist_update = function(socketId, files_send)
-            {
-                connection.emit('fileslist.update', socketId, JSON.stringify(files_send));
-            }
+            // Files list request
             connection.fileslist_request = function(socketId)
             {
                 connection.emit('fileslist.request', socketId);
             }
-			connection.transfer_query_chunk = function(socketId, filename, chunk)
-	        {
-			    connection.emit('transfer.query_chunk', socketId, filename, chunk);
-			}
+
+            // Files list update
+            connection.on('fileslist.update', function(socketId, data)
+            {
+                host.fileslist_update(socketId, JSON.parse(data))
+            })
+            connection.fileslist_update = function(socketId, files_send)
+            {
+                connection.emit('fileslist.update', socketId, JSON.stringify(files_send));
+            }
+
+			// Transfer
+
+            // Transfer query_chunk
+            connection.on('transfer.query_chunk', host.transfer_query_chunk)
+            connection.transfer_query_chunk = function(socketId, filename, chunk)
+            {
+                connection.emit('transfer.query_chunk', socketId, filename, chunk);
+            }
+
+            // Transfer send_chunk
+			connection.on('transfer.send_chunk', function(socketId, filename, chunk, data)
+			{
+				host.transfer_send_chunk(socketId, filename, parseInt(chunk), data)
+			})
+            connection.transfer_send_chunk = function(socketId, filename, chunk, data)
+            {
+                connection.emit('transfer.send_chunk', socketId, filename, chunk, data);
+            }
 
 			if(onsuccess)
 				onsuccess(connection);
