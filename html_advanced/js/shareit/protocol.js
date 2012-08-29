155ba5fe8dec9c37362dbed06bcea65a77542c73
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-08-29 23:03:42 +0200

    Resorted functions

--- html_advanced/js/shareit/protocol.js
+++ html_advanced/js/shareit/protocol.js
@@ -18,38 +18,38 @@ function Conn_init(ws_url, host, onconnect, onsuccess, onerror)
 				onconnect(socket);
 
 			// Files list
-
-            // Files list query
             socket.fileslist_query = function(socketId)
             {
                 socket.emit('fileslist.query', socketId);
             }
-            socket.on('fileslist.query', host.fileslist_query)
-            socket.on('fileslist.query.error', host.fileslist_query_error)
-
-            // Files list update
             socket.fileslist_send = function(socketId, fileslist)
             {
                 socket.emit('fileslist.send', socketId, fileslist);
             }
-            socket.on('fileslist.send', host.fileslist_send)
-//            socket.on('fileslist.send.error', host.fileslist_send_error)
 
-			// Transfer
-
-            // Transfer query
+            // Transfer
             socket.transfer_query = function(socketId, filename, chunk)
             {
                 socket.emit('transfer.query', socketId, filename, chunk);
             }
-            socket.on('transfer.query', host.transfer_query)
-//            socket.on('transfer.query.error', host.transfer_query_error)
-
-            // Transfer send
             socket.transfer_send = function(socketId, filename, chunk, data)
             {
                 socket.emit('transfer.send', socketId, filename, chunk, data);
             }
+
+            // Files list query
+            socket.on('fileslist.query', host.fileslist_query)
+            socket.on('fileslist.query.error', host.fileslist_query_error)
+
+            // Files list update
+            socket.on('fileslist.send', host.fileslist_send)
+//            socket.on('fileslist.send.error', host.fileslist_send_error)
+
+            // Transfer query
+            socket.on('transfer.query', host.transfer_query)
+//            socket.on('transfer.query.error', host.transfer_query_error)
+
+            // Transfer send
             socket.on('transfer.send', host.transfer_send)
 //            socket.on('transfer.send.error', host.transfer_send_error)
 
