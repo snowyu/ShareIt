350ae961001b361b2b3003003f378fbbba920309
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-09-04 20:11:57 +0200

    Some integration

--- html_advanced/js/shareit/host.js
+++ html_advanced/js/shareit/host.js
@@ -12,20 +12,20 @@ var chunksize = 65536
 
 function Bitmap(size)
 {
-	var bitmap = new Array(size)
-	for(var i=0; i<size; i++)
-		bitmap[i] = i;
-	return bitmap
+  var bitmap = new Array(size)
+  for(var i=0; i<size; i++)
+    bitmap[i] = i;
+  return bitmap
 }
 
 function getRandom(bitmap)
 {
-	return bitmap[Math.floor(Math.random() * bitmap.length)]
+  return bitmap[Math.floor(Math.random() * bitmap.length)]
 }
 
 function remove(bitmap, item)
 {
-    bitmap.splice(bitmap.indexOf(item), 1)
+  bitmap.splice(bitmap.indexOf(item), 1)
 }
 
 
