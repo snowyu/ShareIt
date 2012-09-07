e0de6d3241aa4cecee96965e594bebe434e86d44
Riceball LEE <snowyu.lee@gmail.com>
2012-09-07 11:15:40 +0800

    * jslint hint

--- html_advanced/js/shareit.js
+++ html_advanced/js/shareit.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function()
 {
     // Init user interface
-    UI_init()
+    UI_init();
 
     // Init database
 	DB_init(function(db)
@@ -9,44 +9,46 @@ window.addEventListener("load", function()
         // Get shared points and init them
         db.sharepoints_getAll(null, function(sharedpoints)
         {
-            ui_update_fileslist_sharedpoints(sharedpoints)
+            ui_update_fileslist_sharedpoints(sharedpoints);
 
             // [To-Do] Start hashing new files on the shared points
-        })
+        });
 
         ui_ready_fileschange(function(sharedpoints)
         {
             // Loop through the FileList and add sharedpoints to list.
             for(var i = 0, sp; sp = sharedpoints[i]; i++)
-                db.sharepoints_add(sp)
+                db.sharepoints_add(sp);
 
             // [To-Do] Start hashing of files in a new worker
 
-            db.sharepoints_getAll(null, ui_update_fileslist_sharedpoints)
-        })
+            db.sharepoints_getAll(null, ui_update_fileslist_sharedpoints);
+        });
 
         // Load websocket connection after IndexedDB is ready
-        Protocol_init(new WebSocket('wss://localhost:8001'),
+        // the security socket connection would raise error for no proper cert.
+        //Protocol_init(new WebSocket('wss://localhost:8001'),
+        Protocol_init(new WebSocket('ws://localhost:8001'),
         function(protocol)
         {
 	        // Init host
 		    Host_init(db, protocol, function(host)
 		    {
-                var ui = UI_setHost(host)
+                var ui = UI_setHost(host);
 
                 db.sharepoints_getAll(null, function(filelist)
                 {
-                    ui.update_fileslist_sharing(filelist)
+                    ui.update_fileslist_sharing(filelist);
 
 //                    // Restard downloads
 //                    for(var i = 0, file; file = filelist[i]; i++)
 //                        if(file.bitmap)
 //                            protocol.emit('transfer.query', file.name,
 //                                                            getRandom(file.bitmap))
-                })
-	        })
+                });
+	        });
 
-            UI_setProtocol(protocol)
-	    })
-	})
-})
+            UI_setProtocol(protocol);
+	    });
+	});
+});
